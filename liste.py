e2a68f4110909f26350bbace86b5a25be5d0ca2c
Veronica Contini <91256566+VerodavveroV@users.noreply.github.com>
2025-09-19 14:21:14 +0200

    Liste

---
+++ liste.py
@@ -0,0 +1,17 @@
+numeri = [0, 1, 2]
+numeri.append(5)
+print(numeri)
+
+frutti = ['mela', 'pera']
+frutti.insert(3, 'orange')
+print(frutti)
+
+numeri = [0, 1, 2]
+numeri.remove(2)
+print(numeri)
+
+numeri = [0, 1, 2, 3, 4]
+numeri.append(5)
+numeri.pop(2)
+numeri.insert(2, 10)
+print(numeri)
