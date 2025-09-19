f0971ba7aecf47fa9574d8ec58c71c450a9e8a5a
Veronica Contini <91256566+VerodavveroV@users.noreply.github.com>
2025-09-19 14:43:38 +0200

    tuple

---
+++ tuple.py
@@ -0,0 +1,15 @@
+giorni = ("lunedì", "martedì", "mercoledì", "giovedì", "venerdì", "sabato", "domenica" )
+print(giorni[0])
+print(giorni[-1])
+for i in giorni:
+    print(i)
+
+x = (5,)
+print(type(x))
+
+colori = ('rosso', 'blu', 'giallo', 'rosso')
+print(colori.count('rosso'))
+
+colorz = ('giallo', 'verde', 'blu')
+print(colorz[1], colorz[-1])
+print(colorz.count('verde'))
