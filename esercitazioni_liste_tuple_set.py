860c57ae43eb95a2bf5ed01cded0746b7908b0a8
Veronica Contini <91256566+VerodavveroV@users.noreply.github.com>
2025-09-22 14:08:36 +0200

    22 Esercitazione

--- esercitazioni_liste_tuple_set.py
+++ esercitazioni_liste_tuple_set.py
@@ -43,3 +43,37 @@ numeri = [12, 3, 45, 7, 9]
 risultato = (min(numeri), max(numeri))
 print(f"Minimo e massimo: {risultato}")
 
+tuples = [(1,3), (2,1), (5,0)]
+ordinata = sorted(tuples, key=lambda x: x[1])
+print(f"Tuple ordinate per secondo elemento: {ordinata}")   
+
+t = (1, 2, 3, 4, 5, 6)
+pari = tuple ([x for x in t if x % 2 == 0])
+print(f"Tuple con numeri pari: {pari}")
+
+t = (1, 2, 3, 4)
+invertita = tuple(reversed(t))
+print(f"Tuple invertita: {invertita}")
+
+s = "programmazione"
+t = tuple(set(s))
+print(f"Tuple di caratteri unici: {t}")
+
+a = [1, 2, 3]
+b = ('a', 'b', 'c')
+zipped = list(zip(a, b))
+print(f"Liste zipate: {zipped}")
+
+a = {1, 2, 3}
+b = {3, 4, 5}
+c = {5,6}
+differenza = a^b^c
+print(f"Differenza simmetrica: {differenza}")
+
+frase = "ciao come stai ciao tutto bene"
+uniche = set(frase.split())
+print(f"Parole uniche: {uniche}")
+
+liste = [[1, 2, 3], [3, 4, 5], [6, 7]]
+unione = set().union(*map(set, liste))
+print(f"Unione di liste: {unione}")
